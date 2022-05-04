016954acd08eebb55653fbcd76a7e39cd090dd02
Cole <colescarter@gmail.com>
2022-05-04 15:05:46 -0700

    Changed skilldemo

---
+++ skillsDemonstration.java
@@ -0,0 +1,14 @@
+class skillsDemo{
+    skillsDemo(){
+        int i;
+        int j;
+        int z;
+    }
+    public int divide(){
+
+        int i = 1;
+        int j = 2;
+        int z = i / j;
+        return z;
+    }
+}
